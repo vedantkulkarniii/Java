ef2679bed90b19d0bbc0a2f92885cbaab6d57437
vedantkulkarniii <vedkulkarni1276@gmail.com>
2026-07-01 11:22:08 +0530

    Add Java notes on keywords, identifiers, and literals

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 40;
-        int sum = a+b;
-        System.out.println(sum);
+      System.out.println("*\n**\n***\n****\n");
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Java
+<!-- # Java
+Keywords in Java 
+int
+double
+class
+public
+private
+static
+void
+if
+else
+for
+while
+return
+new
+this
+super
+try
+catch
+throw
+extends
+implements -->
+
