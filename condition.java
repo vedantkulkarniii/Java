1de0045b3628b3f65f249252df5514eb49d3226b
vedantkulkarniii <vedkulkarni1276@gmail.com>
2026-07-07 18:36:33 +0530

    Odd Even

--- condition.java
+++ condition.java
@@ -3,15 +3,15 @@ public class condition {
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-
-        if(age>18){
-            System.out.println("Adult");
+        int x = sc.nextInt();
+        if(x%2==0){
+            System.out.println("Even");
         }
-        else {
-            System.out.println("Not Adult");
+        else{
+            System.out.println("odd");
         }
 
+        
     }
     
 }
