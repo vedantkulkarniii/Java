78122b5434ff9a98a1e6c9d89461a6835b51f04d
vedantkulkarniii <vedkulkarni1276@gmail.com>
2026-07-07 13:50:16 +0530

    Conditional Statement

---
+++ First.java
@@ -0,0 +1,12 @@
+public class First{
+
+    public static void main(String[] args) {
+
+        int age = 18;
+
+        if(age >= 18){
+            System.out.println("Eligible");
+        }
+    }
+}
+
