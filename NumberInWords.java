846ff8537ea60c2e55d6e13f4bb1c6cf06ed90b8
vedantkulkarniii <vedkulkarni1276@gmail.com>
2026-07-08 16:39:36 +0530

    NumberstoWords

--- NumberInWords.java
+++ NumberInWords.java
@@ -4,44 +4,59 @@ public class NumberInWords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number (0-9): ");
+        System.out.print("Enter a number (0-9999): ");
         int num = sc.nextInt();
 
-        switch (num) {
-            case 0:
-                System.out.println("Zero");
-                break;
-            case 1:
-                System.out.println("One");
-                break;
-            case 2:
-                System.out.println("Two");
-                break;
-            case 3:
-                System.out.println("Three");
-                break;
-            case 4:
-                System.out.println("Four");
-                break;
-            case 5:
-                System.out.println("Five");
-                break;
-            case 6:
-                System.out.println("Six");
-                break;
-            case 7:
-                System.out.println("Seven");
-                break;
-            case 8:
-                System.out.println("Eight");
-                break;
-            case 9:
-                System.out.println("Nine");
-                break;
-            default:
-                System.out.println("Invalid input");
+        if (num < 0 || num > 9999) {
+            System.out.println("Invalid input! Please enter a number between 0 and 9999.");
+        } else {
+            System.out.println("In words: " + convertToWords(num));
         }
 
         sc.close();
+    }
+
+    public static String convertToWords(int num) {
+        String[] ones = {
+            "", "One", "Two", "Three", "Four", "Five",
+            "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+            "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+        };
+
+        String[] tens = {
+            "", "", "Twenty", "Thirty", "Forty", "Fifty",
+            "Sixty", "Seventy", "Eighty", "Ninety"
+        };
+
+        if (num == 0) {
+            return "Zero";
+        }
+
+        String result = "";
+
+        // Thousands
+        if (num >= 1000) {
+            result += ones[num / 1000] + " Thousand ";
+            num %= 1000;
+        }
+
+        // Hundreds
+        if (num >= 100) {
+            result += ones[num / 100] + " Hundred ";
+            num %= 100;
+        }
+
+        // Tens and Ones
+        if (num >= 20) {
+            result += tens[num / 10] + " ";
+            num %= 10;
+        }
+
+        if (num > 0) {
+            result += ones[num];
+        }
+
+        return result.trim();
     }
 }
