783f736a2bdcfc47659df8c5f31b0d8aea8b9e6d
vedantkulkarniii <vedkulkarni1276@gmail.com>
2026-07-08 19:54:01 +0530

    Switch case in java

--- ATMmachine.java
+++ ATMmachine.java
@@ -5,29 +5,18 @@ public class ATMmachine {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter amount to withdraw: ");
-        int amount = sc.nextInt();
+        int button = sc.nextInt();
 
-        if (amount % 50 != 0) {
-            System.out.println("Invalid amount! Please enter amount in multiples of 50.");
-        } else {
-            int note500 = amount / 500;
-            amount = amount % 500;
-
-            int note200 = amount / 200;
-            amount = amount % 200;
-
-            int note100 = amount / 100;
-            amount = amount % 100;
-
-            int note50 = amount / 50;
-            amount = amount % 50;
-
-            System.out.println("500 notes = " + note500);
-            System.out.println("200 notes = " + note200);
-            System.out.println("100 notes = " + note100);
-            System.out.println("50 notes = " + note50);
+        switch (button){
+            case 1: System.out.println("Helloo");
+            break;
+            case 2: System.out.println("Namastee");
+            break;
+            case 3 : System.out.println("Bonjur");
+            break;
+            default: System.out.println("Invalid button");
         }
 
-        sc.close();
+
     }
 }
