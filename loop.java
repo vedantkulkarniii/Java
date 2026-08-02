5e59e0f1458e223ccceef61fee7477ef53975d49
Vedant Kulkarni <vedantkulkarniii@github.com>
2026-08-02 14:20:33 +0530

    While

--- loop.java
+++ loop.java
@@ -3,8 +3,13 @@ public class loop {
         // for(int counter = 0; counter <11; counter++)
         //     System.out.println(counter);
 
-        for (int dick = 0; dick < 11; dick++)
-            System.out.println(dick);
+        // for (int dick = 0; dick < 11; dick++)
+        //     System.out.println(dick);
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
         
     }
 }
