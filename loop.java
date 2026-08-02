296170bfe538ba084c805ea4bbbdd6757fa393fe
Vedant Kulkarni <vedantkulkarniii@github.com>
2026-08-02 14:24:44 +0530

    While

--- loop.java
+++ loop.java
@@ -1,15 +1,12 @@
 public class loop {
     public static void main(String[] args){
-        // for(int counter = 0; counter <11; counter++)
-        //     System.out.println(counter);
-
-        // for (int dick = 0; dick < 11; dick++)
-        //     System.out.println(dick);
         int i = 0;
-        while(i<11){
+        do{
             System.out.println(i);
-            i++;
+            i = i+ 1;
+
+        } while(i < 11);
         }
         
-    }
+    
 }
