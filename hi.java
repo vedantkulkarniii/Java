5fc1a8c209153eaff4e380b5a67d45aee99506b9
Vedant Kulkarni <vedantkulkarniii@github.com>
2026-08-02 14:10:59 +0530

    Loop

--- hi.java
+++
@@ -1,7 +0,0 @@
-public class hi {
-    public static void main(String[] args){
-      for (int i=1; i<5; i++){
-        System.out.println("***** ");
-      }
-    }
-}
---
+++ loop.java
@@ -0,0 +1,10 @@
+public class loop {
+    public static void main(String[] args){
+        // for(int counter = 0; counter <11; counter++)
+        //     System.out.println(counter);
+
+        for (int dick = 0; dick < 11; dick++)
+            System.out.println(dick);
+        
+    }
+}
