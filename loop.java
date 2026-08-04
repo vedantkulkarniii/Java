c38fd8f9bec65dd2bb5d89045aa9087b8c8c3b79
vedantkulkarniii <vedkulkarni1276@gmail.com>
2026-08-04 11:38:30 +0530

    Meta

--- loop.java
+++ loop.java
@@ -1,11 +1,6 @@
-import java.util.*;
 public class loop {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for(int i=1; i<11; i++){
-            System.out.println(i*n);
-        }
+    public static void main(String[] args){
+        System.out.println("Hello World");
     }
+    
 }
